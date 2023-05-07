6c5d3ecedf7abf94655f6981239a0619fef94b21
KitchenTable99 <ccbitt23@colby.edu>
2023-05-07 18:15:41 -0400

    add git abbreviations

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,15 +2,22 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# abbr
-abbr c 'clear'
-abbr n 'nvim'
-abbr tn "tmux new -s (pwd | sed 's/.*\///g')"
-abbr lg 'lazygit'
-
 # initialize cli helpers
 starship init fish | source
 zoxide init fish | source
 
 # set env_vars
 set -gx DOTFILES /Users/cbitting/Documents/Coding/dotfiles
+
+# abbr
+abbr c 'clear'
+abbr n 'nvim'
+abbr tn "tmux new -s (pwd | sed 's/.*\///g')"
+
+# git 
+abbr lg 'lazygit'
+abbr gp 'git push'
+abbr gl 'git pull'
+abbr gst 'git status'
+abbr gcam 'git commit -a -m'
+abbr gcdc 'git commit --allow-empty -m "dummy commit"'
