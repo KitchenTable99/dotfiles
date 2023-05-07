77c0f699489763223b60be7aada3ddf51afbcb23
KitchenTable99 <ccbitt23@colby.edu>
2023-05-07 17:59:18 -0400

    extract switching to subcommand

--- fish/.config/fish/functions/dotfiles.fish
+++ fish/.config/fish/functions/dotfiles.fish
@@ -1,34 +1,36 @@
 function dotfiles
-  if test (count $argv) -eq 0
+  if test (count $argv) -ne 1
     echo "incorrect usage!"
-    echo "dotfiles [load | remove] [light]"
+    echo "dotfiles [load | remove | switch]"
     exit 1
   end
 
   set current_dir (pwd)
   cd $DOTFILES
 
-  switch $argv[2]
-    case "light"
-      yes | fish_config theme save "latte"
-      git checkout light_mode
-    case "*"
-      yes | fish_config theme save "frappe"
-      git checkout mainline
-  end
-
-  git fetch
-  git pull
-
   switch $argv[1]
     case "load"
+      git fetch
+      git pull
       for package in */
         stow --dir=$DOTFILES --target=$HOME --verbose --restow $package
       end
+
     case "remove"
       for package in */
         stow --dir=$DOTFILES --target=$HOME --verbose --delete $package
       end
+
+    case "switch"
+      set current_branch (git symbolic-ref --short HEAD)
+      if test $current_branch = "mainline"
+        yes | fish_config theme save "latte"
+        git checkout light_mode
+      else
+        yes | fish_config theme save "frappe"
+        git checkout mainline
+      end
+
     case "*"
       echo "Command not supported ($argv)"
   end
