6af4f5ae5d88c5cc9101b0000c1552e76a2f2e97
KitchenTable99 <ccbitt23@colby.edu>
2023-05-14 12:25:23 -0400

    run nexus once per day

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -23,3 +23,26 @@ abbr gst 'git status'
 abbr gcam 'git commit -a -m'
 abbr gcdc 'git commit --allow-empty -m "dummy commit"'
 abbr gco 'git checkout'
+
+function should_run_nexus
+    set -l last_run_file last_nexus_run
+    set -l current_date (date "+%Y-%m-%d")
+
+    if not test -e $last_run_file
+        echo $current_date > $last_run_file
+        return 0
+    end
+
+    set -l last_run_date (cat $last_run_file)
+
+    if test $last_run_date != $current_date
+        echo $current_date > $last_run_file
+        return 0
+    end
+
+    return 1
+end
+
+if should_run_nexus
+  nexus
+end
