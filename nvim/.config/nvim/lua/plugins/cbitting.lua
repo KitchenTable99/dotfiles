5845b826d9bb823ace599165cd3153d7e2673bf6
Bitting <ccbitt@3c22fba0a413.ant.amazon.com>
2023-10-20 10:00:14 -0700

    Add NinjaHooks

--- nvim/.config/nvim/lua/plugins/cbitting.lua
+++ nvim/.config/nvim/lua/plugins/cbitting.lua
@@ -337,6 +337,25 @@ return {
   },
 
   {
+    url = "ccbitt@git.amazon.com:pkg/NinjaHooks",
+    lazy = false,
+    branch = "mainline",
+    config = function(plugin)
+      vim.opt.rtp:prepend(plugin.dir .. "/configuration/vim/amazon/brazil-config")
+      -- Make my own filetype thing to override neovim applying ".conf" file type.
+      -- You may or may not need this depending on your setup.
+      vim.filetype.add({
+        filename = {
+          ["Config"] = function()
+            vim.b.brazil_package_Config = 1
+            return "brazilconfig"
+          end,
+        },
+      })
+    end,
+  },
+
+  {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
       -- add tsx and treesitter
