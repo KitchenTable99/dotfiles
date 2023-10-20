5d52ec2546a6112aad0534410b4b3b0c56c43414
Bitting <ccbitt@3c22fba0a413.ant.amazon.com>
2023-10-20 10:03:24 -0700

    [Amazon] LSP support

--- nvim/.config/nvim/lua/plugins/cbitting.lua
+++ nvim/.config/nvim/lua/plugins/cbitting.lua
@@ -372,11 +372,32 @@ return {
     "neovim/nvim-lspconfig",
     ---@class PluginLpsOpts
     opts = {
+      autoformat = false,
       ---@type lspconfig.options
       servers = {
-        -- pyright will be automatically installed with mason and loaded with lspconfig
         pyright = {},
+        kotlin_language_server = {},
+        barium = {},
       },
+      setup = {
+        -- barium for brazil-config completion
+        -- https://w.amazon.com/bin/view/Barium
+        -- toolbox install barium
+        barium = function()
+          local lspconfig = require("lspconfig")
+          local configs = require("lspconfig.configs")
+          configs.barium = {
+            default_config = {
+              cmd = { "barium" },
+              filetypes = { "brazilconfig" },
+              root_dir = function(fname)
+                return lspconfig.util.find_git_ancestor(fname)
+              end,
+              settings = {},
+            },
+          }
+        end,
+      }
     },
   },
 }
