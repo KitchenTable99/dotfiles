29430a9802fd0377d18cd536460be692c61e8d10
Caleb Bitting <ccbitt@amazon.com>
2024-08-01 13:32:04 -0700

    Update work

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,6 +5,7 @@ end
 # initialize cli helpers
 starship init fish | source
 zoxide init fish | source
+atuin init fish --disable-up-arrow | source
 
 # set env_vars
 set -gx DOTFILES /Users/ccbitt/Documents/03\ Resources/dotfiles
